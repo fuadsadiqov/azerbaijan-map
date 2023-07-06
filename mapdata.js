54f2e7f3bedc2f0d47aa63f09e402f0f58c3d514
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-07-06 10:52:58 +0400

    first commit

---
+++ mapdata.js
@@ -0,0 +1,401 @@
+
+var simplemaps_countrymap_mapdata={
+    main_settings: {
+     //General settings
+      width: "500", //'700' or 'responsive'
+      background_color: "#FFFFFF",
+      background_transparent: "yes",
+      border_color: "#222222",
+      
+      //State defaults
+      state_description: "State description",
+      state_color: "#EEF2FD",
+      state_hover_color: "#3B729F",
+      state_url: "",
+      border_size: 1.5,
+      all_states_inactive: "no",
+      all_states_zoomable: "yes",
+      
+      //Location defaults
+      location_description: "Location description",
+      location_url: "",
+      location_color: "#FF0067",
+      location_opacity: 0.8,
+      location_hover_opacity: 1,
+      location_size: 25,
+      location_type: "square",
+      location_image_source: "frog.png",
+      location_border_color: "#FFFFFF",
+      location_border: 2,
+      location_hover_border: 2.5,
+      all_locations_inactive: "no",
+      all_locations_hidden: "no",
+      
+      //Label defaults
+      label_color: "#d5ddec",
+      label_hover_color: "#d5ddec",
+      label_size: 22,
+      label_font: "Arial",
+      hide_labels: "no",
+      hide_eastern_labels: "no",
+     
+      //Zoom settings
+      zoom: "yes",
+      manual_zoom: "yes",
+      back_image: "no",
+      initial_back: "no",
+      initial_zoom: "-1",
+      initial_zoom_solo: "no",
+      region_opacity: 1,
+      region_hover_opacity: 0.6,
+      zoom_out_incrementally: "yes",
+      zoom_percentage: 0.99,
+      zoom_time: 0.5,
+      
+      //Popup settings
+      popup_color: "white",
+      popup_opacity: 0.9,
+      popup_shadow: 1,
+      popup_corners: 5,
+      popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+      popup_nocss: "no",
+      
+      //Advanced settings
+      div: "map",
+      auto_load: "yes",
+      url_new_tab: "no",
+      images_directory: "default",
+      fade_time: 0.1,
+      link_text: "View Website",
+      popups: "detect",
+      state_image_url: "",
+      state_image_position: "",
+      location_image_url: ""
+    },
+    state_specific: {
+      AZE1676: {
+        name: "Ağstafa",
+        description: " "
+      },
+      AZE1677: {
+        name: "Daşkəsən",
+        description: " "
+      },
+      AZE1678: {
+        name: "Gədəbəy",
+        description: " "
+      },
+      AZE1679: {
+        name: "Gəncə",
+        description: " "
+      },
+      AZE1680: {
+        name: "Xanlar",
+        description: " "
+      },
+      AZE1681: {
+        name: "Goranboy",
+        description: " "
+      },
+      AZE1682: {
+        name: "Kəlbəcər",
+        description: " "
+      },
+      AZE1683: {
+        name: "Mingəçevir",
+        description: " "
+      },
+      AZE1684: {
+        name: "Qazax",
+        description: " "
+      },
+      AZE1685: {
+        name: "Şəmkir",
+        description: " "
+      },
+      AZE1686: {
+        name: "Samux",
+        description: " "
+      },
+      AZE1687: {
+        name: "Tovuz",
+        description: " "
+      },
+      AZE1688: {
+        name: "Yevlakh Rayon",
+        description: " "
+      },
+      AZE1689: {
+        name: "Bakı",
+        description: " "
+      },
+      AZE1690: {
+        name: "Abşeron",
+        description: " "
+      },
+      AZE1691: {
+        name: "Ağdam",
+        description: " "
+      },
+      AZE1692: {
+        name: "Ağdaş",
+        description: " "
+      },
+      AZE1693: {
+        name: "Ağsu",
+        description: " "
+      },
+      AZE1694: {
+        name: "Hajigabul",
+        description: " "
+      },
+      AZE1695: {
+        name: "Astara",
+        description: " "
+      },
+      AZE1696: {
+        name: "Bərdə",
+        description: " "
+      },
+      AZE1697: {
+        name: "Beyləqan",
+        description: " "
+      },
+      AZE1698: {
+        name: "Biləsuvar",
+        description: " "
+      },
+      AZE1699: {
+        name: "Cəbrayıl",
+        description: " "
+      },
+      AZE1700: {
+        name: "Cəlilabad",
+        description: " "
+      },
+      AZE1701: {
+        name: "Dəvəçi",
+        description: " "
+      },
+      AZE1702: {
+        name: "Füzuli",
+        description: " "
+      },
+      AZE1703: {
+        name: "Göyçay",
+        description: " "
+      },
+      AZE1704: {
+        name: "İmişli",
+        description: " "
+      },
+      AZE1705: {
+        name: "İsmayıllı",
+        description: " "
+      },
+      AZE1706: {
+        name: "Kürdəmir",
+        description: " "
+      },
+      AZE1707: {
+        name: "Lankaran",
+        description: " "
+      },
+      AZE1708: {
+        name: "Masallı",
+        description: " "
+      },
+      AZE1709: {
+        name: "Lerik",
+        description: " "
+      },
+      AZE1710: {
+        name: "Neftçala",
+        description: " "
+      },
+      AZE1711: {
+        name: "Qobustan",
+        description: " "
+      },
+      AZE1712: {
+        name: "Siyəzən",
+        description: " "
+      },
+      AZE1713: {
+        name: "Saatlı",
+        description: " "
+      },
+      AZE1714: {
+        name: "Sabirabad",
+        description: " "
+      },
+      AZE1715: {
+        name: "Salyan",
+        description: " "
+      },
+      AZE1716: {
+        name: "Şamaxı",
+        description: " "
+      },
+      AZE1717: {
+        name: "Sumqayıt",
+        description: " "
+      },
+      AZE1718: {
+        name: "Ucar",
+        description: " "
+      },
+      AZE1719: {
+        name: "Xizı",
+        description: " "
+      },
+      AZE1720: {
+        name: "Yardımlı",
+        description: " "
+      },
+      AZE1721: {
+        name: "Zərdab",
+        description: " "
+      },
+      AZE1722: {
+        name: "Ağcabədi",
+        description: " "
+      },
+      AZE1723: {
+        name: "Balakən",
+        description: " "
+      },
+      AZE1724: {
+        name: "Qəbələ",
+        description: " "
+      },
+      AZE1725: {
+        name: "Oğuz",
+        description: " "
+      },
+      AZE1726: {
+        name: "Qax",
+        description: " "
+      },
+      AZE1727: {
+        name: "Şəki",
+        description: " "
+      },
+      AZE1728: {
+        name: "Quba",
+        description: " "
+      },
+      AZE1729: {
+        name: "Qusar",
+        description: " "
+      },
+      AZE1730: {
+        name: "Xaçmaz",
+        description: " "
+      },
+      AZE1731: {
+        name: "Zaqatala",
+        description: " "
+      },
+      AZE1734: {
+        name: "Xocavənd",
+        description: " "
+      },
+      AZE1735: {
+        name: "Lankaran",
+        description: " "
+      },
+      AZE1736: {
+        name: "Qubadli",
+        description: " "
+      },
+      AZE1737: {
+        name: "Şuşa",
+        description: " "
+      },
+      AZE1738: {
+        name: "Tərtər",
+        description: " "
+      },
+      AZE1739: {
+        name: "Xocalı",
+        description: " "
+      },
+      AZE1740: {
+        name: "Zəngilan",
+        description: " "
+      },
+      AZE2415: {
+        name: "Sədərək",
+        description: " "
+      },
+      AZE2418: {
+        name: "Ordubad",
+        description: " "
+      },
+      AZE2419: {
+        name: "Şərur",
+        description: " "
+      },
+      AZE2420: {
+        name: "Babək",
+        description: " "
+      },
+      AZE2421: {
+        name: "Culfa",
+        description: " "
+      },
+      AZE2422: {
+        name: "Naxçıvan",
+        description: " "
+      },
+      AZE2423: {
+        name: "Şahbuz",
+        description: " "
+      },
+      AZE4838: {
+        name: "Stepanakert",
+        description: " "
+      },
+      AZE5561: {
+        name: "Naftalan",
+        description: " "
+      },
+      AZE5562: {
+        name: "Lankaran",
+        description: " "
+      },
+      AZE5563: {
+        name: "Shirvan",
+        description: " "
+      },
+      AZE5564: {
+        name: "Şəki",
+        description: " "
+      },
+      AZE5565: {
+        name: "Şuşa",
+        description: " "
+      },
+      AZE5566: {
+        name: "Yevlakh",
+        description: " "
+      },
+      AZE5567: {
+        name: "Kangarli",
+        description: " "
+      }
+    },
+    locations: {
+      "0": {
+        lat: "40.395278",
+        lng: "49.882222",
+        name: "Baku"
+      }
+    },
+    labels: {},
+    legend: {
+      entries: []
+    },
+    regions: {}
+  };
